c01c0aedbf4dbb5e1c0a750b053d7a6b25976385
Dilla <122979935+Dillaayh@users.noreply.github.com>
2025-07-14 13:20:54 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import LandingPage from './pages/landingpage';
 import DaftarTeknisiPage from './pages/destopteknisi';
 import DaftarInstrumenPage from './pages/instrumen';
 import LogbookPage from './pages/logbook';
-import LogbookPagiPage from './pages/logbookPagi';
+import LogbookPagiPage from "./pages/logbookPagi";
 import Perka from './pages/perka';
 import PerkaMekanik from './pages/perkaMekanik';
 import PerkaCanggih from './pages/perkaCanggih';
 import PerkaSederhana from './pages/perkaSederhana';
-
-
 // import Login from './component/login';
 
 function App() {
@@ -21,13 +19,11 @@ function App() {
         <Route path="/destopteknisi" element={<DaftarTeknisiPage />} />
         <Route path="/instrumen" element={<DaftarInstrumenPage />} />
         <Route path="/logbook" element={<LogbookPage />} />
-        <Route path='/logbook-pagi' element={<LogbookPagiPage />} />
+        <Route path="/logbookPagi" element={<LogbookPagiPage />} />
         <Route path="/perka" element={<Perka />} />
         <Route path="/perkaMekanik" element={<PerkaMekanik />} />
         <Route path="/perkaCanggih" element={<PerkaCanggih />} />
         <Route path="/perkaSederhana" element={<PerkaSederhana />} />
-
-
         {/* <Route path="/login" element={<Login />} /> */}
         <Route path="*" element={<div>404 - Halaman tidak ditemukan</div>} />
       </Routes>
